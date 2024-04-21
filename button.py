07f9b1ad7d9b24ba65a3d6db6b3742f3dd5fcbd0
Bilal <bilal.ayachi@hetmml.nl>
2024-04-21 14:21:28 +0200

    [remove] comments

--- button.py
+++ button.py
@@ -1,6 +1,6 @@
 import pygame
 
-class Button: # Source: https://www.youtube.com/watch?v=4_9twnEduFA
+class Button: # Source: https://www.youtube.com/watch?v=4_9twnEduFA  (Minor adjustements have been made to the class to make it appealing for me to use)
     def __init__(self, color, x, y, width, height, textSize, text=''):
         self.color = color
         self.x = x
@@ -11,7 +11,6 @@ class Button: # Source: https://www.youtube.com/watch?v=4_9twnEduFA
         self.textSize = textSize
 
     def draw(self, win, outline=None):
-        # Call this method to draw the button on the screen
         if outline:
             pygame.draw.rect(win, outline, (self.x-2, self.y-2, self.width+4, self.height+4), 0)
             
@@ -23,7 +22,6 @@ class Button: # Source: https://www.youtube.com/watch?v=4_9twnEduFA
             win.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
 
     def isOver(self, pos):
-        # Pos is the mouse position or a tuple of (x, y) coordinates
         if pos[0] > self.x and pos[0] < self.x + self.width:
             if pos[1] > self.y and pos[1] < self.y + self.height:
                 return True
