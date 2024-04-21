d032e8bf13435bd0b1c38e7c82d9c06b675edcce
Bilal <bilal.ayachi@hetmml.nl>
2024-04-21 13:49:27 +0200

    [add] hover effect on buttons

--- menu.py
+++ menu.py
@@ -12,15 +12,21 @@ pygame.font.init()
 pygame.init()
 
 run = True
-gameScreen = pygame.font.SysFont('Comic Sans MS', 100)
+gameScreen = pygame.font.SysFont('Comic Sans MS', 200)
 
-morePlayerMode = button.Button((255, 255, 255),SCREEN_HIGHT/2,SCREEN_HIGHT/4,800,100,'8 player')
-singleMode = button.Button((255, 255, 255),SCREEN_HIGHT/2,SCREEN_HIGHT/2.5,800,100,'1v1')
-quitTheGame = button.Button((255, 255, 255),SCREEN_HIGHT/2,SCREEN_HIGHT/1.75,800,100,'Quit')
+# Press  (164,146,163). Hover (218,211,218).
+
+buttonColor8 = (226,221,220) 
+buttonColorVersus = (226,221,220) 
+buttonColorQiut = (226,221,220) 
 
 
 while run:
-    screen.fill((0,0,0))
+    morePlayerMode = button.Button(buttonColor8,SCREEN_WIDTH/3.5,SCREEN_HIGHT/4,800,100,'8 player')
+    singleMode = button.Button(buttonColorVersus,SCREEN_WIDTH/3.5,SCREEN_HIGHT/2.5,800,100,'1v1')
+    quitTheGame = button.Button(buttonColorQiut,SCREEN_WIDTH/3.5,SCREEN_HIGHT/1.75,800,100,'Quit')
+
+    screen.fill((153,0,17))
     # Sumo rama welcome
     gameScreen_surface = gameScreen.render('Sumo Rama', True, (255, 255, 255))
     gameScreen_rect = gameScreen_surface.get_rect(center=(SCREEN_WIDTH/2, 100))
@@ -29,6 +35,19 @@ while run:
     singleMode.draw(screen, (0,0,0))
     morePlayerMode.draw(screen, (0,0,0))
     quitTheGame.draw(screen, (0,0,0))
+
+    pos = pygame.mouse.get_pos()
+    if singleMode.isOver(pos):
+        buttonColorVersus = (183,179,183) 
+    elif morePlayerMode.isOver(pos):
+        buttonColor8 = (183,179,183)
+    elif quitTheGame.isOver(pos):
+        buttonColorQiut = (183,179,183)
+    else:
+        buttonColorVersus = (226,221,220)
+        buttonColor8 = (226,221,220)
+        buttonColorQiut = (226,221,220)
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -43,8 +62,6 @@ while run:
                 print("Player quit the game")
                 run = False
 
-
-    button.Button((100,100,100),300,300,100,100,"heyy")
 
     pygame.display.update()
     clock.tick(60)  # Limit to 60 FPS
