34ef4c1035f8a2a54ada5e44333f6d6724d1c9f2
Bilal <bilal.ayachi@hetmml.nl>
2024-04-29 20:36:21 +0200

    [add] countdown before game starts

--- versusArena.py
+++ versusArena.py
@@ -1,6 +1,5 @@
 import pygame
 import random
-import player
 import threading
 import time
 import json
@@ -11,184 +10,188 @@ FPS = 60
 SCREEN_WIDTH = 1300
 SCREEN_HEIGHT = 800
 
-#TODO: Add lockstep simulation, test 1v1 throughly, Add IP for HOST
-
 class VersusArena:
     def __init__(self, screen, gameState, player, peer, gameOver):
         pygame.init()
         pygame.font.init()
+
+        self.gameFont = pygame.font.SysFont('Comic Sans MS', 40)
+
         self.clock = pygame.time.Clock()
 
-        self.screen = screen  # Set display resolution
+        self.screen = screen
         self.gameState = gameState
         self.peer = peer
         self.player = player
         self.gameOver = gameOver
 
-        self.player_circle = {"position": [0,0], "velocity": [0,0], "radius": 40, "name": self.player.getName()} # Contains information about the sumo of the player. e.g position, speed, radius, name
+        self.player_circle = {"position": [0,0], "velocity": [0,0], "radius": 40, "name": self.player.getName()}
+        self.enemy_circle = {"position": [0,0], "velocity": [0,0], "radius": 40, "name": None}
 
-        self.enemy_circle = {"position": [0,0], "velocity": [0,0], "radius": 40, "name": None} # Contains information about the sumo enemy of the player. e.g position, speed, radius, name
+        self.sumo_ring_radius = 450
+        self.sumo_ring_center = [SCREEN_WIDTH/2, SCREEN_HEIGHT/2]
 
-        # Define sumo ring properties
-        self.sumo_ring_radius = 450  # Larger radius for the sumo ring
-        self.sumo_ring_center = [SCREEN_WIDTH/2, SCREEN_HEIGHT/2]  # Center of the screen (1920x1080 resolution) 
+        self.circle_radius = 40 # Radius of the player
 
-        self.circle_radius = 40  # Initial radius of all circles
-
-        # Define rush variables
-        self.rush_duration = 0.5  # Rush duration in seconds
-        self.rush_speed = 300  # Rush speed in pixels per second
+        #States for rushing
+        self.rush_duration = 0.5
+        self.rush_speed = 300
         self.rushing = False
         self.rush_start_time = 0
 
-        # Define shrink variables
+        # States of circle shrink
         self.shrink_timer = 0
-        self.shrink_interval = 5  # Time interval in seconds to shrink the sumo ring
-        self.shrink_scale = 0.9  # Scaling factor for shrinking the sumo ring
+        self.timerScreen = 10
+        self.shrink_interval = 10
+        self.shrink_scale = 0.9
 
-        #States
+        # Game STates
         self.gameStateRun = True
         self.playerPositionInit = None
-        self.winnerOfTheGame = None # Stores the winner
+        self.winnerOfTheGame = None
 
-    def setPeer(self, peer):
+    def setPeer(self, peer): # Setter for peer
         self.peer = peer
 
-
-    def listenForData(self): # Listens for data, other player
+    def listenForData(self): # Listens for data from peer
         while True:
-            data, addr = self.peer.getSocket().recvfrom(65535) # Max udp size
+            data, addr = self.peer.getSocket().recvfrom(65535)
             data = data.decode()
 
-            if "INIT-OK" == data:
+            if "INIT-OK" == data: # Ack from peer
                 self.playerPositionInit = True
-            elif "INIT" in data: # We get start positions from other peer
-                print(data)
+            elif "INIT" in data: # Init handeling
                 self.enemy_circle['position'] = [int(data.split(" ")[1]), int(data.split(" ")[2])]
                 self.enemy_circle['name'] = data.split(" ")[3]
                 self.peer.getSocket().sendto("INIT-OK".encode(), addr)
-            elif "UPDATE" in data:
-                newData = json.loads(data.split(" ",1)[1]) # Format; UPDATE {NEWDATA}
-                #print(newData)
+            elif "UPDATE" in data: # New data from peer
+                newData = json.loads(data.split(" ",1)[1])
                 self.enemy_circle = newData
 
-    def sendInitPositions(self, data):
+    def sendInitPositions(self, data): # Sends init position to other peer
         while True:
             if self.playerPositionInit == True:
                 break
             self.peer.getSocket().sendto(data.encode(), list(self.peer.getConnections())[0])
-            time.sleep(0.1)
-
+            time.sleep(0.1) # Sends every 0.1 seconds
 
     def initPositions(self):
-        randomPointInRing = self.randomPointInCircle(self.sumo_ring_radius-(0.3 * self.sumo_ring_radius), self.sumo_ring_center[0], self.sumo_ring_center[1]) # --> (x,y)
+        randomPointInRing = self.randomPointInCircle(self.sumo_ring_radius-(0.3 * self.sumo_ring_radius), self.sumo_ring_center[0], self.sumo_ring_center[1])
+        self.player_circle['position'] = [math.ceil(randomPointInRing[0]),math.ceil(randomPointInRing[1])]
 
-        self.player_circle['position'] = [math.ceil(randomPointInRing[0]),math.ceil(randomPointInRing[1])] # init positions 
-
-        payload = "INIT " + str(math.ceil(randomPointInRing[0])) + " " + str(math.ceil(randomPointInRing[1])) + " " + self.player.getName() # Protocol: INIT playerStartPositon.x playerStartPosition.y NAME 
+        payload = "INIT " + str(math.ceil(randomPointInRing[0])) + " " + str(math.ceil(randomPointInRing[1])) + " " + self.player.getName()
 
         sendInit_thread = threading.Thread(target=self.sendInitPositions,args=(payload,), daemon=True)
         sendInit_thread.start()
 
-
-    def randomPointInCircle(self, radius, centerX, centerY):
-        # random angle
+    def randomPointInCircle(self, radius, centerX, centerY): # Uses circle formula to find a random point in a circle
         alpha = 2 * math.pi * random.random()
-        # random radius
         r = radius * math.sqrt(random.random())
-        # calculating coordinates
         x = r * math.cos(alpha) + centerX
         y = r * math.sin(alpha) + centerY
         return (x,y)
 
     def rush_to_cursor(self):
-        # Get current mouse position
         mouse_x, mouse_y = pygame.mouse.get_pos()
-
-        # Calculate direction vector from player circle to cursor
         if self.player_circle is not None:
             direction = [mouse_x - self.player_circle["position"][0], mouse_y - self.player_circle["position"][1]]
             length = math.sqrt(direction[0]**2 + direction[1]**2)
-
             if length > 0:
-                # Normalize direction vector
                 direction = [direction[0] / length, direction[1] / length]
-
-                # Calculate rush movement towards cursor
                 self.player_circle["velocity"][0] = direction[0] * self.rush_speed
                 self.player_circle["velocity"][1] = direction[1] * self.rush_speed
 
     def handle_collision(self, circle1, circle2):
         distance = math.sqrt((circle1["position"][0] - circle2["position"][0])**2 +
                             (circle1["position"][1] - circle2["position"][1])**2)
-        if distance < 2 * self.circle_radius:  # Check collision with diameter (2 * radius)
-            # Calculate overlap and direction of collision
+        
+
+        if distance < 2 * self.circle_radius:
             overlap = 2 * self.circle_radius - distance
             collision_direction = [circle2["position"][0] - circle1["position"][0],
                                 circle2["position"][1] - circle1["position"][1]]
+            
             collision_length = math.sqrt(collision_direction[0]**2 + collision_direction[1]**2)
 
             if collision_length > 0:
-                # Normalize collision direction
                 collision_direction = [collision_direction[0] / collision_length,
                                     collision_direction[1] / collision_length]
-                
-                # Resolve collision by pushing the circles away
                 move_distance = overlap / 2
+
                 circle1["position"][0] -= move_distance * collision_direction[0]
                 circle1["position"][1] -= move_distance * collision_direction[1]
                 circle2["position"][0] += move_distance * collision_direction[0]
                 circle2["position"][1] += move_distance * collision_direction[1]
 
-
     def checkIfGameOver(self):
-        # Check and handle player circle
         if self.player_circle is not None:
             player_distance_to_center = math.sqrt((self.player_circle["position"][0] - self.sumo_ring_center[0])**2 +
                                                 (self.player_circle["position"][1] - self.sumo_ring_center[1])**2)
+            
+
             if player_distance_to_center + self.player_circle["radius"] > self.sumo_ring_radius:
                 self.winnerOfTheGame = self.enemy_circle['name']
-                return True # Game Over
+                return True
 
-        # Checks if other 1v1 peer is out of circle
         distance_to_center = math.sqrt((self.enemy_circle["position"][0] - self.sumo_ring_center[0])**2 +
                                         (self.enemy_circle["position"][1] - self.sumo_ring_center[1])**2)
+        
         if distance_to_center + self.enemy_circle["radius"] > self.sumo_ring_radius:
             self.winnerOfTheGame = self.player.getName()
             return True
-       
-        return False # Not out of circle
-    
-    def resetStates(self):
-        self.player_circle = {"position": [0,0], "velocity": [0,0], "radius": 40, "name": self.player.getName()} # Contains information about the sumo of the player. e.g position, speed, radius, name
-        self.enemy_circle = {"position": [0,0], "velocity": [0,0], "radius": 40, "name": None} # Contains information about the sumo enemy of the player. e.g position, speed, radius, name
 
+        return False
+
+    def resetStates(self):
+        self.player_circle = {"position": [0,0], "velocity": [0,0], "radius": 40, "name": self.player.getName()}
+        self.enemy_circle = {"position": [0,0], "velocity": [0,0], "radius": 40, "name": None}
         self.gameStateRun = True
         self.playerPositionInit = None
-        self.winnerOfTheGame = None # Stores the winner
+        self.winnerOfTheGame = None
+        self.sumo_ring_radius = 450
 
-        self.sumo_ring_radius = 450  # resets sumo ring
+    def displayCountdown(self):
+        countdown_font = pygame.font.SysFont('Comic Sans MS', 100)
+        for i in range(5, 0, -1):  # Countdown from 5 to 1 seconds
+            self.screen.fill((255, 255, 255))  # Clear the screen, white countdown
+
+            countdown_text = countdown_font.render(str(i), True, (0, 0, 0))
+            text_rect = countdown_text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
+            self.screen.blit(countdown_text, text_rect)
+            
+            pygame.display.update()
+            pygame.time.wait(1000)  # Wait every 1 second so counter goes down per second
 
     def run(self):
-        self.resetStates() # REset previous game states
+        self.resetStates() # Reset previous game states
 
-        # start listinng thread
+        # Start listening thread
         recv_thread = threading.Thread(target=self.listenForData, daemon=True)
         recv_thread.start()
 
-        print(self.peer.getConnections())
+        #print(self.peer.getConnections())
 
-        self.initPositions() # Inits positions from all peers
+        self.initPositions() 
 
-        while self.gameStateRun:
-            self.screen.fill((255,255,255)) # White screen
+        #print(self.playerPositionInit)
 
-            if self.checkIfGameOver() == True and self.playerPositionInit == True: # self.playerPositionInit makes sure players are loaded in before checking if game over
+        self.displayCountdown() # Assures sync between clients
+
+        while self.gameStateRun and self.playerPositionInit:
+            #print(self.playerPositionInit)
+
+            self.screen.fill((255, 255, 255)) # White screen arena
+
+            if self.checkIfGameOver() == True and self.playerPositionInit == True:
                 print("Game over")
                 self.gameStateRun = False
                 self.gameState.setCurrentState('1v1GameOver')
                 self.gameOver.setWinner(self.winnerOfTheGame)
-                
+
+            gameScreen_waveTimer = self.gameFont.render('0:' + str(self.timerScreen), True, (0, 0, 0))
+            gameScreen_rect = gameScreen_waveTimer.get_rect(center=(SCREEN_WIDTH - 30, 20))
+            self.screen.blit(gameScreen_waveTimer, gameScreen_rect)
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.gameStateRun = False
@@ -199,19 +202,17 @@ class VersusArena:
                         self.rushing = True
                         self.rush_start_time = pygame.time.get_ticks()
 
-            # Handle rush movement towards cursor
             if self.rushing and self.player_circle is not None:
                 current_time = pygame.time.get_ticks()
-                if current_time - self.rush_start_time < self.rush_duration * 1000:  # Check if still within rush duration
+                if current_time - self.rush_start_time < self.rush_duration * 1000:
                     self.rush_to_cursor()
                 else:
-                    self.rushing = False  # Stop rushing after duration expires
-                    self.player_circle["velocity"] = [0, 0]  # Stop the player circle
+                    self.rushing = False
+                    self.player_circle["velocity"] = [0, 0]
 
             pygame.draw.circle(self.screen, self.player.getColor(), (self.player_circle['position'][0],self.player_circle['position'][1]),40)
             pygame.draw.circle(self.screen, self.player.getColor(), (self.enemy_circle['position'][0],self.enemy_circle['position'][1]),40)
 
-             # Handle player input (move player circle)
             keys = pygame.key.get_pressed()
             if keys[pygame.K_a]:
                 self.player_circle['position'][0] -= 3
@@ -229,20 +230,17 @@ class VersusArena:
                 self.player_circle["position"][0] += self.player_circle["velocity"][0] * self.clock.get_time() / 1000
                 self.player_circle["position"][1] += self.player_circle["velocity"][1] * self.clock.get_time() / 1000
 
-             # Shrink the sumo ring every 5 seconds
-            self.shrink_timer += self.clock.get_time() / 1000  # Convert milliseconds to seconds
+            self.shrink_timer += self.clock.get_time() / 1000
             if self.shrink_timer >= self.shrink_interval:
-                self.sumo_ring_radius *= self.shrink_scale  # Shrink the sumo ring
-                self.shrink_timer = 0  # Reset shrink timer
+                self.sumo_ring_radius *= self.shrink_scale
+                self.shrink_timer = 0
+                self.timerScreen = 10
 
-            # Draw sumo ring boundary   
             pygame.draw.circle(self.screen, (255, 0, 0), self.sumo_ring_center, int(self.sumo_ring_radius), 3)
 
             peerPositionJSON = json.dumps(self.player_circle)
-
-            payload = "UPDATE " + peerPositionJSON            
-
-            self.peer.getSocket().sendto(payload.encode(), list(self.peer.getConnections())[0]) 
+            payload = "UPDATE " + peerPositionJSON
+            self.peer.getSocket().sendto(payload.encode(), list(self.peer.getConnections())[0])
 
             pygame.display.update()
-            self.clock.tick(FPS)  # Limits FPS#
+            self.clock.tick(FPS)
