7a0a09bcc6ca7f8da2366a3339c92fd084c24036
Bilal <bilal.ayachi@hetmml.nl>
2024-05-02 18:55:10 +0200

    [add] very simple server

--- src/8player/rendezvous.py
+++ src/8player/rendezvous.py
@@ -1,1 +1,18 @@
-#TODO Add rendezvous server and a working lobby.
+import socket
+import threading
+
+SERVER_ADDRESS = '127.0.0.1'
+SERVER_PORT = 5378
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+sock.bind((SERVER_ADDRESS, SERVER_PORT))
+
+sock.listen()
+
+print("Rendezvous server is online")
+
+while True:
+    client_socket, address = sock.accept()
+    print(f'{address}' + " connected to match making sever")
+    
